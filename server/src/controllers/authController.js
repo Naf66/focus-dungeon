27ff9a2c84950f3f0f9f52d3fa2693d11821f73f
Naf66 <mohaiminnafe@gmail.com>
2026-06-09 17:53:29 +0600

    refactore routes with controller

---
+++ server/src/controllers/authController.js
@@ -0,0 +1,48 @@
+import pool from "../config/db.js";
+import bcrypt from "bcrypt";
+
+export const registerUser = async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password,10);
+
+  await pool.query(
+    `
+    INSERT INTO users (username, email, password)
+    VALUES ($1, $2, $3)
+    `,
+    [username, email, hashedPassword]
+  );
+
+  res.json({
+    message: "User registered"
+  });
+}
+
+export const loginUser = async(req,res)=>{
+    const {email,password}=req.body;
+    const result = await pool.query(
+        `
+        SELECT * FROM users WHERE email = $1
+        `,
+        [email]
+    )
+    const user =result.rows[0];
+    if(!user){
+        return res.status(404).json({
+            message: "User not found"
+        })
+    }
+    const isMatch = await bcrypt.compare(
+        password,
+        user.password
+    )
+    if(!isMatch){
+        res.status(401).json({
+            message: "Invalid credentials"
+        })
+    }
+
+    res.json({
+        message:"Login successfull"
+    })
+}
--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,55 +1,12 @@
 import express from "express";
 import pool from "../config/db.js"
 import bcrypt from "bcrypt"
+import {registerUser,loginUser} from "../controllers/authController.js";
 
 const router =express.Router();
 
 
-router.post("/register", async (req, res) => {
-  const { username, email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password,10);
-
-  await pool.query(
-    `
-    INSERT INTO users (username, email, password)
-    VALUES ($1, $2, $3)
-    `,
-    [username, email, hashedPassword]
-  );
-
-  res.json({
-    message: "User registered"
-  });
-});
-
-
-router.post("/login", async(req,res)=>{
-    const {email,password}=req.body;
-    const result = await pool.query(
-        `
-        SELECT * FROM users WHERE email = $1
-        `,
-        [email]
-    )
-    const user =result.rows[0];
-    if(!user){
-        return res.status(404).json({
-            message: "User not found"
-        })
-    }
-    const isMatch = await bcrypt.compare(
-        password,
-        user.password
-    )
-    if(!isMatch){
-        res.status(401).json({
-            message: "Invalid credentials"
-        })
-    }
-
-    res.json({
-        message:"Login successfull"
-    })
-})
+router.post("/register", registerUser );
+router.post("/login",loginUser);
 
 export default router;
